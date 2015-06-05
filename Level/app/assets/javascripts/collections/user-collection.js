f2ae04f8f9d21c201504b8710a2b1decaa309a7e
Meir <meir@Meirs-Mac.local>
2015-06-05 18:11:29 -0400

    backbone

--- Level/app/assets/javascripts/collections/user-collection.js
+++ Level/app/assets/javascripts/collections/user-collection.js
@@ -1,5 +1,10 @@
 // Basic Backbone Collection for Users
 
+// Ajax prefileter
+$.ajaxPrefilter( function( options, originalOptions, jqxhr ) {
+	options.url = "http://localhost:3000" + options.url;
+});
+
 var UserCollection = Backbone.Collection.extend({
   model: User,
   url: '/users'
--- Level/app/assets/javascripts/routers/user-router.js
+++ Level/app/assets/javascripts/routers/user-router.js
@@ -1,1 +1,21 @@
-user-router.js
+var Router = Backbone.Router.extend({
+   routes: {
+      'users': 'index',
+  }
+});
+
+var rescueData = new RescueData();
+
+
+ index: function() {
+      var users = new UsersCollection();
+      window.usersView = new UsersCollectionView({
+        el: $(".main"),
+        collection: users
+      });
+    },
+
+$(function(){
+	window.router = new UsersRouter(),
+	Backbone.history.start()
+})
