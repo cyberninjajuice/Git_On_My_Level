15d4a1dedd2b4ebfb56e0b4f25b4be0fd5394c20
Raz <razgoldin@gmail.com>
2015-06-08 12:33:33 -0400

    working on D3

--- Level/app/assets/javascripts/views/view.js
+++ Level/app/assets/javascripts/views/view.js
@@ -167,7 +167,7 @@ console.log("view");
         success: function(model, response){
           console.log(model)
           this.template = _.template($('#skill-template').html());
-          console.log(this.template)
+          //console.log(this.template)
           console.log(thisView.render());
           thisView.render();
         },
@@ -181,9 +181,14 @@ console.log("view");
     render: function() {
       console.log("rendering!")
       var el = this.$el;
-      //clear the content-area.
-      //$('div#content-area').empty();
       el.empty();
+      // clear the content-area.
+      //$('div#content-area').empty();
+
+
+      console.log(this.collection)
+      //render a SkillView for each skill
+      
       el.append("<h1>Skills</h1>");
       this.collection.each(function(skill) {
         el.append(new SkillView({model: skill}).render().el);
@@ -191,6 +196,81 @@ console.log("view");
 
       // add the view to the content-area
       $('#content-area').html(el);
+      var data = $("#user-chart").first().text();
+      console.log(data);
+      data = JSON.parse(data)
+      console.log(data.length);
+      data.map(function(i){
+        console.log(i);
+      })
+      console.log(d3)
+      var chart = document.getElementById("chart");
+      console.log(chart)
+      var axisMargin = 20, margin = 20, valueMargin = 4, width = chart.offsetWidth, height = chart.offsetHeight, barHeight = (height-axisMargin-margin*2)* 0.4/data.length, barPadding = (height-axisMargin-margin*2)* 0.6/data.length, data, bar, svg, scale, xAxis, labelWidth = 0;
+
+      var max = d3.max(data.map(function(i){ 
+        return i[1];
+      }));
+      
+      svg = d3.select(chart)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", 400);
+
+
+      bar = svg.selectAll("g")
+        .data(data)
+        .enter()
+        .append("g");
+
+      bar.attr("class", "bar")
+        .attr("cx",0)
+        .attr("transform", function(d, i) { 
+           return "translate(" + margin + "," + (i * (barHeight + barPadding) + barPadding) + ")";
+        });
+
+      bar.append("text")
+        .attr("class", "label")
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em") //vertical align middle
+        .text(function(d){
+          return d[0];
+        }).each(function() {
+          labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+        });
+
+      scale = d3.scale.linear()
+        .domain([0, max])
+        .range([0, width - margin*2 - labelWidth]);
+
+      bar.append("rect")
+        .attr("transform", "translate("+labelWidth+", 0)")
+        .attr("height", barHeight)
+        .attr("width", function(d){
+          console.log("face");
+          
+          return scale(d[1]);
+        });
+
+      bar.append("text")
+        .attr("class", "value")
+        .attr("y", barHeight / 2)
+        .attr("dx", -valueMargin + labelWidth) //margin right
+        .attr("dy", ".35em") //vertical align middle
+        .attr("text-anchor", "end")
+        .text(function(d){
+          return d[1];
+        })
+        .attr("x", function(d){
+          var width = this.getBBox().width;
+          return Math.max(width + valueMargin, scale(d[1]));
+       });
+
+      svg.insert("g",":first-child")
+       .attr("class", "axis")
+       .attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin)+")")
+       .call(xAxis);
+
       return this;
     }
 
