aafa8a47ffd1f171bfd76d39765dbdc692f1d64c
Raz <razgoldin@gmail.com>
2015-06-16 00:32:07 -0400

    changing worker amounts

--- Level/config/puma.rb
+++ Level/config/puma.rb
@@ -1,11 +1,10 @@
 threads 1, 6
-workers 2
+workers 4
 
 on_worker_boot do
   require "active_record"
   cwd = File.dirname(__FILE__)+"/.."
   ActiveRecord::Base.connection.disconnect! 
-  binding.pry
   rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RAILS_ENV"]])
 end
